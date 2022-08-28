10492090f30fbcecd171a0a739aacfac81e0af97
Luiz Felipe <luiz.souza@tech4h.com.br>
2022-08-27 21:07:05 -0300

    inclusao do terceiro teste e tratamento de erros

--- src/clients/StormGlass.ts
+++ src/clients/StormGlass.ts
@@ -1,5 +1,5 @@
-import { AxiosStatic } from 'axios';
-
+import axios, { AxiosError, AxiosStatic } from 'axios';
+import { InternalError } from '@src/util/errors/internal-error';
 export interface StormGlassPointSource {
   [key: string]: number;
 }
@@ -30,18 +30,50 @@ export interface ForecastPoint {
   windSpeed: number;
 }
 
+export class ClientRequestError extends InternalError {
+  constructor(message: string) {
+    const internalMessage =
+      'Unexpected error when trying to communicate to StormGlass';
+    super(`${internalMessage}: ${message}`);
+  }
+}
+
+export class StormGlassResponseError extends InternalError {
+  constructor(message: string) {
+    const internalMessage =
+      'Unexpected error returned by the StormGlass service';
+    super(`${internalMessage}: ${message}`);
+  }
+}
 export class StormGlass {
   readonly stormGlassAPIParams =
     'swellDirection,swellHeight,swellPeriod,waveDirection,waveHeight,windDirection,windSpeed';
   readonly stormGlassAPISource = 'noaa';
 
-  constructor(protected request: AxiosStatic) {}
+  constructor(protected request: AxiosStatic = axios) {}
 
   public async fetchPoints(lat: number, lng: number): Promise<ForecastPoint[]> {
+    try{
     const response = await this.request.get<StormGlassForecastResponse>(
-      `https://api.stormglass.io/v2/weather/point?params=${this.stormGlassAPIParams}&source=${this.stormGlassAPISource}&end=1592113802&lat=${lat}&lng=${lng}`
+      `https://api.stormglass.io/v2/weather/point?params=${this.stormGlassAPIParams}&source=${this.stormGlassAPISource}&end=1592113802&lat=${lat}&lng=${lng}`,
+      {
+        headers: {
+          Authorization: 'fake token',
+      },
+    }
     );
     return this.normalizeResponse(response.data);
+  }  catch (err: unknown) {
+    if ((err as AxiosError).response && (err as AxiosError).response?.data) {
+      throw new StormGlassResponseError(
+        `Error: ${JSON.stringify((err as AxiosError).response?.data)} Code: ${
+          (err as AxiosError).response?.status
+        }`
+      );
+    }
+
+    throw new ClientRequestError((err as Error).message);
+  }
   }
 
   private normalizeResponse(
--- src/clients/__test__/StormGlass.test.ts
+++ src/clients/__test__/StormGlass.test.ts
@@ -6,15 +6,51 @@ import stormGlassNormalizedResponse3HoursFixtures from '@test/fixtures/stormglas
 jest.mock('axios');
 
 describe('StormGlass client', () => {
+  const mockedAxios = axios as jest.Mocked<typeof axios>;
   it('should return the normalized forecast from the StormGlass service', async () => {
     const lat = -33.792726;
     const lng = 151.289824;
 
-    axios.get = jest
-      .fn()
-      .mockResolvedValue({ data: stormGlassWeather3HoursFixtures });
-    const stormGlass = new StormGlass(axios);
+    mockedAxios.get.mockResolvedValue({
+      data: stormGlassWeather3HoursFixtures,
+    });
+
+    const stormGlass = new StormGlass(mockedAxios);
     const response = await stormGlass.fetchPoints(lat, lng);
     expect(response).toEqual(stormGlassNormalizedResponse3HoursFixtures);
   });
+
+  it('should exclude incomplete data points', async () => {
+    const lat = -33.792726;
+    const lng = 151.289824;
+    const incompleteResponse = {
+      hours: [
+        {
+          windDirection: {
+            noaa: 300,
+          },
+          time: '2022-08-27T02:00:00+00:00',
+        },
+      ],
+    };
+    mockedAxios.get.mockResolvedValue({ data: incompleteResponse });
+
+    const stormGlass = new StormGlass(mockedAxios);
+    const response = await stormGlass.fetchPoints(lat, lng);
+    expect(response).toEqual([]);
+  });
+
+  it('should get a generic error from StormGlass service when the request fail before reaching the service', async () => {
+    const lat = -33.792726;
+    const lng = 151.289824;
+
+    mockedAxios.get.mockRejectedValue({ message: 'Network Error' });
+
+    const stormGlass = new StormGlass(mockedAxios);
+
+    await expect(stormGlass.fetchPoints(lat, lng)).rejects.toThrow(
+      'Unexpected error when trying to communicate to StormGlass: Network Error'
+    );
+  });
+  
 });
